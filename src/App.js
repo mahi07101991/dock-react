5a9ab6a6220e89a45fa481e4cfed4492d4d054ff
Mahesh kumar k <mahi07101991@gmail.com>
2023-12-03 23:50:49 +0000

    Second Commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      counter: 0
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log()
+  }
   render() {
     return <div className="App">
       <h1>Hellow World</h1>
+      <button onClick={() => this.setState({ counter: this.state.counter + 100 })}>Click me</button>
+      <h1>{this.state.counter}</h1>
     </div>
   }
 }
